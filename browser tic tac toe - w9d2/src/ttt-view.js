dc85027e575a4bce3090ef7fd4b185641356907b
Ming Zhang <mzhang630@gmail.com>
2022-11-08 17:40:39 -0500

    finished tic tac toe browser

--- browser tic tac toe - w9d2/src/ttt-view.js
+++ browser tic tac toe - w9d2/src/ttt-view.js
@@ -10,16 +10,72 @@ class View {
     let figure = document.getElementsByClassName("ttt");
     let ul = figure[0].appendChild(document.createElement("ul"));
     ul.classList.add("grid");
-    for (let i = 0; i < 9; i++){
-      ul.appendChild(document.createElement("li"));
+    for (let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++){
+        let li = document.createElement('li');
+        li.setAttribute('data-pos', `[${i},${j}]`);
+        ul.appendChild(li);
+      }
     }
+    // el = ul
+    // for (let i = 0; i < 9; i++){
+    //   ul.appendChild(document.createElement("li"));
   }
   
-  bindEvents() {}
+  bindEvents() {
+    // document.getElementsByClassName('grid')[0]
+    let ul = document.querySelector('ul');
+    
+    ul.addEventListener('click', (e) => {
+      this.handleClick(e);
+    })
+  }
 
-  handleClick(e) {}
+  handleClick(e) {
+    let li = e.target;
+    this.makeMove(li);
+  }
 
-  makeMove(square) {}
+  makeMove(square) {
+
+   let strVal = square.getAttribute('data-pos')
+  //  console.log(strVal)
+   
+   let pos = [parseInt(strVal[1]), parseInt(strVal[3])]
+  //  console.log(pos)
+  //  console.log(pos, pos[0])
+  //  console.log(square.getAttribute('data-pos')[0])
+  //  console.log(this.game)
+  //  debugger
+   this.game.playMove(pos)
+   
+  //  console.log(pos);
+  // debugger
+  // const listItems = document.querySelectorAll(".grid > li")
+  
+  let currentPlayer = this.game.currentPlayer
+
+  // console.log(square)
+    // debugger
+  if (currentPlayer === 'x'){
+    square.innerHTML = 'x';
+    square.style.backgroundColor = 'blue';
+    console.log(square.innerHTML)
+  } else {
+    square.innerHTML = 'o';
+    square.style.backgroundColor = 'red';
+    console.log(square.innerHTML)
+  }
+
+  // Element.style.backgroundColor = 'blue';
+
+  if (this.game.board.isOver()){
+    this.game.winner();
+    alert('game over')
+    
+    }
+
+  }
 
 }
 
